cd6e48cafc55310770863498b7f7191847e3f1df
Nicholas Lim <nicholas.lim@elastic.co>
2020-07-19 16:12:36 +0800

    Docker for Ubuntu install and change to NodePort for localVM tests

---
+++ install/install-ubuntu.sh
@@ -0,0 +1,40 @@
+#!/bin/sh -xvf
+
+sudo apt-get update
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates wget curl software-properties-common gnupg-agent
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+# Install minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
+sudo install minikube /usr/local/bin
+
+## Install docker for Ubuntu
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee -a /etc/apt/sources.list.d/docker.list
+sudo apt update
+sudo apt-cache policy docker-ce
+sudo apt-get -y install docker-ce
+sudo systemctl start docker
+
+ME=$(whoami)
+#
+# # Make sure it is running as admin user
+# if [ $ME != "admin" ]
+# then
+#    echo not admin
+#    sudo deluser admin
+#    sudo adduser --home /home/admin --disabled-password --disabled-login --quiet --gecos "" admin
+#    sudo usermod -aG sudo admin
+#    sudo gpasswd -a admin docker
+#    sudo cp -R $HOME/k8s-o11y-workshop /home/admin
+#    sudo chown -R admin:admin /home/admin/k8s-o11y-workshop
+#    sudo chmod -R ogu+rw /home/admin/k8s-o11y-workshop
+#    sudo su - admin
+# fi
+
+sudo usermod -g docker $ME
+
+newgrp - docker
